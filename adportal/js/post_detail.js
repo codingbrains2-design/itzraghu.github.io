f3e32dd4c29f5941f3a549f09a59a15f54e10eb7
Raghvendra Pratap Singh <raghvendrasngh0@gmail.com>
2015-06-29 16:55:08 +0530

    Create post_detail.js

---
+++ adportal/js/post_detail.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+	
+	$.ajax({
+		url: 'php/post_detail.php',
+		type: 'GET',
+		success: function (data) {
+			var result = $.parseJSON(data);
+			$.each(result, function(key, value) {
+
+				$.each(value, function(key, value) {
+
+					if (key === 'id') { $('#id').append(value); };
+					if (key === 'title') { $('#title').append(value); };
+					if (key === 'price') { $('#price').append(value); };
+					if (key === 'model') { $('#model').append(value); };
+					if (key === 'description') { $('#description').append(value); };
+					if (key === 'name') { $('#name').append(value); };
+					if (key === 'email') { $('#email').append(value); };
+					if (key === 'phone') { $('#phone').append(value); };
+					if (key === 'city') { $('#city').append(value); };
+					if (key === 'path') { $('#path').append(value); };
+				});
+			});
+
+		}
+	});
+
+});
