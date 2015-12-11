cd25fa9fb27a9369880b2698ce1d1a976c55c5b4
itzraghu <raghvendrasngh0@gmail.com>
2015-12-11 14:10:51 +0530

    removed adportal

--- adportal/admin/add-categories.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'database.php';
-
-$category = $_POST['category'];
-$subcategory = $_POST['subcategory'];
-
-$stmt = $dbh->prepare("INSERT INTO categories VALUES (?,?)");
-$stmt->execute(array($category, $subcategory));
-echo "data inserted";
--- adportal/php/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-try{
-	$dbh = new PDO('mysql:host=localhost;dbname=ad_portal', "root" , "");
-	
-
-}catch(PDOException $e){
-	print "error in connection" . $e->getMessage();
-}
